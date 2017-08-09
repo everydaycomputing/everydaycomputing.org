369e37379ebdc195a4fb8761a73db0cdfc30c73c
noah-banholzer <nbanholzer@gmail.com>
2017-08-09 11:59:06 -0500

    changes to learning goal page

--- backend_tools/visualization.py
+++ backend_tools/visualization.py
@@ -134,20 +134,24 @@ class UnderstandingGoalsHandler(webapp2.RequestHandler):
         node_entity = ndb.Key(urlsafe=node_key).get()
         node_ulgs = node_entity.understanding_learning_goals
         node_uuid = node_entity.uuid
+        node_cg = node_entity.understanding
         trajectory_entity = ndb.Key(urlsafe=trajectory_key).get()
         trajectory_title = trajectory_entity.name
         understanding_lgs = []
         for lg in node_ulgs:
             lg_text = lg.get().text
             lg_support = lg.get()._support
-            lg_article_key = ndb.Key(urlsafe=lg.get().goal).get().article.urlsafe()
+            lg_key = ndb.Key(urlsafe=lg.get().goal)
+            lg_article_key = lg_key.get().article.urlsafe()
+            lg_evidence = lg_key.get()._support
+            lg_support = lg_evidence + " - " + lg_support
             understanding_lgs.append([lg_text, lg_support, lg_article_key])
         template_values = {
             'lgs' : understanding_lgs,
             'trajectory_key' : trajectory_key,
             'node_key' : node_key,
             'node_uuid' : node_uuid,
-            'trajectory_title' : trajectory_title
+            'trajectory_title' : node_cg
         }
         template = JINJA_ENVIRONMENT.get_template('templates/public_node_lg_list.html')
         self.response.write(template.render(template_values, name=template_values))
@@ -158,20 +162,24 @@ class ActionGoalsHandler(webapp2.RequestHandler):
         node_entity = ndb.Key(urlsafe=node_key).get()
         node_algs = node_entity.action_learning_goals
         node_uuid = node_entity.uuid
+        node_cg = node_entity.action
         trajectory_entity = ndb.Key(urlsafe=trajectory_key).get()
         trajectory_title = trajectory_entity.name
         action_lgs = []
         for lg in node_algs:
             lg_text = lg.get().text
             lg_support = lg.get()._support
-            lg_article_key = ndb.Key(urlsafe=lg.get().goal).get().article.urlsafe()
+            lg_key = ndb.Key(urlsafe=lg.get().goal)
+            lg_article_key = lg_key.get().article.urlsafe()
+            lg_evidence = lg_key.get()._support
+            lg_support = lg_evidence + " - " + lg_support
             action_lgs.append([lg_text, lg_support, lg_article_key])
         template_values = {
             'lgs' : action_lgs,
             'trajectory_key' : trajectory_key,
             'node_key' : node_key,
             'node_uuid' : node_uuid,
-            'trajectory_title' : trajectory_title
+            'trajectory_title' : node_cg
         }
         template = JINJA_ENVIRONMENT.get_template('templates/public_node_lg_list.html')
         self.response.write(template.render(template_values, name=template_values))
